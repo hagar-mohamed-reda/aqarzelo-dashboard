3e585ba9e8d20b41e84ab7911bed438d2b9a9c4b
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-10-18 16:01:12 +0200

    company

--- resources/views/company/user/action.blade.php
+++ resources/views/company/user/action.blade.php
@@ -1,9 +1,9 @@
 <div style="width: 120px" >
     @if (Auth::user()->_can('edit user'))
-    <i class="fa fa-edit w3-text-orange w3-button" onclick="edit('{{ url('/admin/user/edit') . '/' . $user->id }}')" ></i>
+    <i class="fa fa-edit w3-text-orange w3-button" onclick="edit('{{ url('/company/user/edit') . '/' . $user->id }}')" ></i>
     @endif
 
     @if (Auth::user()->_can('remove user'))
-    <i class="fa fa-trash w3-text-red w3-button" onclick="remove('', '{{ url('/admin/user/remove/') .'/' . $user->id }}')" ></i>
+    <i class="fa fa-trash w3-text-red w3-button" onclick="remove('', '{{ url('/company/user/remove/') .'/' . $user->id }}')" ></i>
     @endif
 </div>
